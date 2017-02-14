17f7e158c2dd35619b973f01d303503fe364d76f
Eric Psalmond <eric@checkr.com>
2017-02-13 21:41:17 -0800

    Cleanup

--- timer.lua
+++ timer.lua
@@ -2,7 +2,7 @@ tmr.create():alarm(1000, tmr.ALARM_AUTO, function()
   new_weight = hx711.read(0)
   print(raw_data)
   http.post(
-  "http://scoopr.herokuapp.com/coffee_weights", "", "coffee_weight%5Bdevice_id%5D=1&coffee_weight%5Bweight%5D=" .. new_weight,function(code, data)
+  "http://scoopr.example.com/coffee_weights", "", "coffee_weight%5Bdevice_id%5D=1&coffee_weight%5Bweight%5D=" .. new_weight,function(code, data)
       if (code < 0) then
         print("HTTP request failed")
       else
